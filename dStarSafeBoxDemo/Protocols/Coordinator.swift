b42eda833fa9df6323e90ef51b990111cba7ef26
Oleg <33181376+Stackmann@users.noreply.github.com>
2021-08-01 15:29:55 +0300

    The very first commit

---
+++ dStarSafeBoxDemo/Protocols/Coordinator.swift
@@ -0,0 +1,10 @@
+//
+//  Coordinator.swift
+//  dStarSafeBoxDemo
+//
+//  Created by admin on 01.08.2021.
+//
+
+protocol Coordinator {
+    func start()
+}
---
+++ dStarSafeBoxDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  dStarSafeBoxDemo
+//
+//  Created by admin on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
